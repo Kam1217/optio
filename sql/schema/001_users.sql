058a46793590022b642486d81babceb7840a3aea
Kam1217 <kamilaostroga@gmail.com>
2025-09-04 10:31:17 +0100

    Users table

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,13 +1,12 @@
 -- +goose Up
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR (250) UNIQUE NOT NULL,
     email VARCHAR (100) UNIQUE NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL 
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL 
 );
 
 CREATE INDEX idx_users_username ON users(username);
