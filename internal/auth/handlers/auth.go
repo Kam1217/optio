1d451b7b816dbe54ef6dc5f25e06675622c7f345
Kam1217 <kamilaostroga@gmail.com>
2025-09-04 10:11:48 +0100

    DB struct

--- internal/auth/handlers/auth.go
+++ internal/auth/handlers/auth.go
@@ -1,16 +1,20 @@
 package handlers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 
-	"github.com/Kam1217/optio/internal/auth/middleware"
 	"github.com/Kam1217/optio/internal/auth/models"
+	"github.com/Kam1217/optio/internal/database"
 )
 
 //This is where the authentication logic goes -login, register etc.
 
-//TODO: DATABASE - sits here
+type DB struct {
+	*sql.DB
+	db *database.Queries
+}
 
 type LoginRequest struct {
 	Username string `json:"username"`
@@ -20,33 +24,33 @@ type LoginRequest struct {
 type RegisterRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
-	Email string `json:"email"`
+	Email    string `json:"email"`
 }
 
 type AuthResponse struct {
-	Token string `json:"token"`
-	User models.User `json:"user"`
+	Token string      `json:"token"`
+	User  models.User `json:"user"`
 }
 
-//Register func 
-//-decode body to struct
-//-Handle empty username, password, email
-//- Check if user exists
-//- generate JWT
-//-Register - create user 
+// Register func
+// -decode body to struct
+// -Handle empty username, password, email
+// - Check if user exists
+// - generate JWT
+// -Register - create user
 func (a *AuthResponse) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	var req RegisterRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w,"Invalid JSON", http.StatusBadRequest)
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
 
 	if req.Username == "" || req.Password == "" || req.Email == "" {
-		http.Error(w,"Username, password and email cannot be empty", http.StatusBadRequest)
-		return 
+		http.Error(w, "Username, password and email cannot be empty", http.StatusBadRequest)
+		return
 	}
 
-	//Check if user exists - db func 
+	//Check if user exists - db func
 
 	//Create user - db func
 
@@ -56,5 +60,5 @@ func (a *AuthResponse) RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 //Login func
 
-//Profile func 
+//Profile func
 //- retrievs the user profile
