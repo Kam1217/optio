2ff91e7e20a6b732b964075ce5116b7ad3d726ec
Kam1217 <kamilaostroga@gmail.com>
2025-09-04 12:43:20 +0100

    connect to db in main

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
 
-func main(){
-	fmt.Print("Hello World! I am making my first big girl app")
+	"github.com/Kam1217/optio/db"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	dbConfig := db.Config{
+		DBName:   os.Getenv("DB_NAME"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+	}
+
+	db, err := db.Connect(dbConfig)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	log.Printf("Succesfully connected to the database")
 }
+
+//Setup mux ports helper func
