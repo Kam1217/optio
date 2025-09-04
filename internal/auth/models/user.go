df42d0dc9cd5907ac1ebd7954492a5854400158f
Kam1217 <kamilaostroga@gmail.com>
2025-09-04 13:21:10 +0100

    User Exists func

--- internal/auth/models/user.go
+++ internal/auth/models/user.go
@@ -1,38 +1,50 @@
 package models
 
 import (
-	"time"
-	"github.com/google/uuid"
+	"context"
+
+	"github.com/Kam1217/optio/internal/database"
 	"golang.org/x/crypto/bcrypt"
 )
 
-// ToDo - User Model with Database
-type User struct {
-	ID       uuid.UUID `json:"id"`
-	Username string    `json:"username"`
-	Password string    `json:"_"`
-	Email    string    `json:"email"`
-	Created_at time.Time `json:"created_at"`
-	Updated_at time.Time `json:"updated_at"`
+type UserService struct {
+	queries *database.Queries
 }
 
-func (u *User) HashPassword(password string) error {
+func NewUserService(queries *database.Queries) *UserService {
+	return &UserService{queries: queries}
+}
+func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	u.Password = string(hashedPassword)
-	return nil
+	return string(hashedPassword), nil
 }
 
-func (u *User) CheckPassword(password string) error {
-	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+func CheckPassword(hashedPassword, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	if err != nil {
 		return err
 	}
 	return nil
 }
+
+func (s *UserService) UserExists(ctx context.Context, username, email string) (bool, error) {
+	usernameExists, err := s.queries.UserExistsByUsername(ctx, username)
+	if err != nil {
+		return false, err
+	}
+
+	emailExists, err := s.queries.UserExistsByEmail(ctx, email)
+	if err != nil {
+		return false, err
+	}
+
+	return usernameExists || emailExists, nil
+}
+
 
 //TODO:Check is user exists
 
@@ -41,5 +53,3 @@ func (u *User) CheckPassword(password string) error {
 //TODO:Get User by username
 
 //TODO:Get User by ID
-
-
