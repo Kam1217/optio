dfd7d62fe6dec7bfab0ccdf9c3de1412b02ad5f5
Kamila Ostroga <kamilaostroga@gmail.com>
2025-09-15 15:09:52 +0100

    change query refresh token name

--- sql/queries/refresh_token.sql
+++ sql/queries/refresh_token.sql
@@ -1,5 +1,5 @@
 -- name: CreateRefreshToken :one
-INSERT INTO refresh_tokens(user_id, token_hash, issued_at, expires_at, user_agent, ip)
+INSERT INTO refresh_token(user_id, token_hash, issued_at, expires_at, user_agent, ip)
 VALUES(
      $1,
      $2,
@@ -12,32 +12,32 @@ RETURNING user_id, token_hash, issued_at, expires_at, revoked_at, user_agent, ip
 
 -- name: GetRefreshTokenByHash :one
 SELECT id, user_id, token_hash, issued_at, expires_at, revoked_at, user_agent, ip
-FROM refresh_tokens
+FROM refresh_token
 WHERE token_hash = $1;
 
 -- name: RevokeRefreshTokenByID :exec
-UPDATE refresh_tokens 
+UPDATE refresh_token 
 SET revoked_at = NOW()
 WHERE id=$1 AND revoked_at IS NULL;
 
 -- name: RevokeAllRefreshTokensForUser :exec
-UPDATE refresh_tokens
+UPDATE refresh_token
 SET revoked_at = NOW()
 WHERE user_id = $1 AND revoked_at IS NULL;
 
 -- name: DeleteExpiredRefreshTokens :exec
-DELETE FROM refresh_tokens
+DELETE FROM refresh_token
 WHERE expires_at < NOW();
 
 -- name: GetActiveRefreshTokenByHash :one
 SELECT id, user_id, token_hash, issued_at, expires_at, revoked_at, user_agent, ip
-FROM refresh_tokens
+FROM refresh_token
 WHERE token_hash = $1
 AND revoked_at IS NULL
 AND expires_at > NOW();
 
 -- name: ListActiveTokensForUser :many
 SELECT id, user_id, issued_at, expires_at, user_agent, ip
-FROM refresh_tokens
+FROM refresh_token
 WHERE user_id = $1 AND revoked_at IS NULL AND expires_at > NOW()
 ORDER BY issued_at DESC;
