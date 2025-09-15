37d20a5de14be3739d959e4fafd633bf2008d70b
Kamila Ostroga <kamilaostroga@gmail.com>
2025-09-15 15:10:06 +0100

    setup session go files

---
+++ app/custom_session.go
@@ -0,0 +1,8 @@
+package app
+
+//Create custom session
+// Title
+//Generate random  invite code - simple
+
+//Add items to session
+// Title, Description, Generated custom images
---
+++ app/steam_session.go
@@ -0,0 +1,5 @@
+package app
+
+//Create custom steam session
+//Pull from the steam api - user library with same games as other users
+//*Toggle all games even if unowned - maybe user is willing to buy / some games are free but not owned
