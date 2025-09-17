d1c602e46cd2c3a69971734112f81ce4d1505a39
Kamila Ostroga <kamilaostroga@gmail.com>
2025-09-17 11:18:12 +0100

    Check session item exists

--- app/session_items.go
+++ app/session_items.go
@@ -2,6 +2,9 @@ package app
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 
 	"github.com/Kam1217/optio/internal/database"
 	"github.com/google/uuid"
@@ -18,8 +21,15 @@ func NewSessionItemService(queries *database.Queries) *SessionItemService {
 	return &SessionItemService{queries: queries}
 }
 
-func (si *SessionItemService) CheckSessionItemExists(ctx context.Context) (bool, error) {
-	return false, nil
+func (si *SessionItemService) CheckSessionItemExists(ctx context.Context, itemID uuid.UUID) (bool, error) {
+	_, err := si.queries.GetSessionItemByID(ctx, itemID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, nil
+		}
+		return false, fmt.Errorf("database error checking session item: %w", err)
+	}
+	return true, nil
 }
 
 func (si *SessionItemService) CreateNewSessionItem(ctx context.Context, sessionID uuid.UUID, itemName, itemDescription, itemImaege string, creatorID uuid.UUID) (*database.SessionItem, error) {
