db03a3808703377a88d9de352a753de26b407247
Kamila Ostroga <kamilaostroga@gmail.com>
2025-09-17 12:40:10 +0100

    Create session item

--- app/session_items.go
+++ app/session_items.go
@@ -3,11 +3,13 @@ package app
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
 
 	"github.com/Kam1217/optio/internal/database"
 	"github.com/google/uuid"
+	"github.com/sqlc-dev/pqtype"
 )
 
 //Check if item already exists in session (by ID) - prevents duplicates
@@ -16,6 +18,50 @@ import (
 type SessionItemService struct {
 	queries *database.Queries
 }
+
+type SourceType string
+
+const (
+	SourceCustom SourceType = "custom"
+	SourceSteam  SourceType = "steam"
+)
+
+//ADD STEAM LATER
+
+type SessionItemInput struct {
+	Title         string
+	Description   string
+	ImageURL      string
+	SessionId     uuid.UUID
+	AddedByUserID uuid.UUID
+	SourceType    SourceType
+	Metadata      json.RawMessage
+}
+
+// type CustomMetadata struct {
+// 	Foo string
+// 	Bar string
+// }
+
+// type SteamMetadata struct {
+// 	Baz int
+// 	Yeet float32
+// }
+
+// func GetMetadata[T any](rawMetadata json.RawMessage) (*T, error) {
+// 	var metadata *T
+// 	err := json.Unmarshal(rawMetadata, metadata)
+// 	return metadata, err
+// }
+
+// func example(sourceType SourceType, rawMetadata pqtype.NullRawMessage) {
+// 	switch sourceType {
+// 	case SourceCustom:
+// 		customMetadata, err := GetMetadata[CustomMetadata](rawMetadata)
+// 	case SourceSteam:
+// 		steamMetadata, err := GetMetadata[SteamMetadata](rawMetadata)
+// 	}
+// }
 
 func NewSessionItemService(queries *database.Queries) *SessionItemService {
 	return &SessionItemService{queries: queries}
@@ -32,6 +78,28 @@ func (si *SessionItemService) CheckSessionItemExists(ctx context.Context, itemID
 	return true, nil
 }
 
-func (si *SessionItemService) CreateNewSessionItem(ctx context.Context, sessionID uuid.UUID, itemName, itemDescription, itemImaege string, creatorID uuid.UUID) (*database.SessionItem, error) {
-	return nil, nil
+func (si *SessionItemService) CreateNewSessionItem(ctx context.Context, itemInput SessionItemInput) (*database.SessionItem, error) {
+	if itemInput.SessionId == uuid.Nil || itemInput.AddedByUserID == uuid.Nil {
+		return nil, fmt.Errorf("missing ID")
+	}
+	//Make switch statement once steam / other is added - //DEFAULT UNSUPORTED SOURCE TYPE
+
+	if itemInput.Title == "" {
+		return nil, fmt.Errorf("title is required")
+	}
+	item, err := si.queries.CreateSessionItem(ctx, database.CreateSessionItemParams{
+		SessionID:       itemInput.SessionId,
+		ItemTitle:       itemInput.Title,
+		ItemDescription: sql.NullString{String: itemInput.Description},
+		ImageUrl:        sql.NullString{String: itemInput.ImageURL},
+		SourceType:      string(SourceCustom),
+		SourceID:        sql.NullString{Valid: false},
+		Metadata:        pqtype.NullRawMessage{RawMessage: itemInput.Metadata},
+		AddedByUserID:   itemInput.AddedByUserID,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create session item: %w", err)
+	}
+
+	return &item, nil
 }
